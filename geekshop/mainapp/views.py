7f5b8d9f040be7cab98f0786378de59b7c069b8f
TRiG <gerysh07@gmail.com>
2020-12-09 13:35:33 +0300

    lesson 2

--- geekshop/mainapp/views.py
+++ geekshop/mainapp/views.py
@@ -2,8 +2,33 @@ from django.shortcuts import render
 
 
 def main(request):
-    return render(request, 'mainapp/index.html')
+    content = {
+        'title': 'GeekShop - Главная'
+    }
+    return render(request, 'mainapp/index.html', content)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    content = {
+        'title': 'GeekShop - Категории'
+    }
+    return render(request, 'mainapp/products.html', content)
+
+
+def test_context(request):
+    """Test function for getting acquainted with the context."""
+    context = {
+        'title': 'Test Context',
+        'header': 'Добро пожловать на сайт!',
+        'username': 'Герман Триг',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals', 'price': '6 090,00 руб.'},
+            {'name': 'Синяя куртка The North Face', 'price': '23 725,00 руб.'},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN', 'price': '3 390,00 руб.'},
+        ],
+        'promotion': True,
+        'products_of_promotion': [
+            {'name': 'Черный рюкзак Nike Heritage', 'price': '2 340,00 руб.'},
+        ]
+    }
+    return render(request, 'mainapp/test_context.html', context)
