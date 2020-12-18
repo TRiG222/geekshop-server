a365b385c4f6aa14e8f0f5a564c2ec3ad330f1aa
TRiG <gerysh07@gmail.com>
2020-12-18 13:45:12 +0300

    lesson 4

--- geekshop/geekshop/urls.py
+++ geekshop/geekshop/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path, include
 from django.contrib import admin
+from mainapp import views as mainapp_views
 from django.conf import settings
 from django.conf.urls.static import static
 
-from mainapp import views as mainapp_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', mainapp_views.main, name='main'),
-    path('products/', include('mainapp.urls', namespace='products')),
+    path('', mainapp_views.main, name='index'),
+    path('products/', mainapp_views.products, name='products'),
+    path('auth/', include('authapp.urls')),
 ]
 
 if settings.DEBUG:
